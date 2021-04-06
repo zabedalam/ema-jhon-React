4e61f086be3d698b8076499767249978e472a7e3
zabed <zabedalam777@gmail.com>
2021-04-06 13:17:54 +0200

    local storage database implemented and show product in review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getDatabaseCart } from "../../utilities/databaseManager";
+
+import fakeData from "../../fakeData";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Review = () => {
-    return (
-        <div>
-            <h1>This is Review Page</h1>
-        </div>
-    );
+    const [cart,setCart]=useState([])
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
+    const cartProducts = productKeys.map(key => {
+      const product = fakeData.find(pd => pd.key === key);
+      product.quantity = savedCart[key];
+
+      return product;
+    });
+setCart(cartProducts)
+    console.log("cart show",cartProducts);
+  },[]);
+  return (
+    <div>
+      <h1>Cart Items:{cart.length}</h1>
+      {cart.map(pd=>(<ReviewItem key={pd.key} product={pd}></ReviewItem>))}
+    </div>
+  );
 };
 
 export default Review;
---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ReviewItem.css'
+
+const ReviewItem = (props) => {
+    const {name,quantity,price}=props.product
+    console.log(props)
+    return (
+        <div className="review-product">
+            <h4 className="product-name">{name}</h4>
+            <p>Quantity:{quantity}</p>
+            <p><small>${price}</small></p>
+            <br/>
+            <button className="btn-main">Remove</button>
+        </div>
+    );
+};
+
+export default ReviewItem;
